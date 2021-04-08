a22e9020cc662d4796205c8602bffd471b059bd4
MarlineKhavele <marlinekhavele6@gmail.com>
2021-04-08 20:57:55 +0300

    added category

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ from products import views
 urlpatterns = [
     path('latest-products/',views.LatestProductList.as_view()),
     path('products/<slug:category_slug>/<slug:product_slug/',views.ProductDetail.as_view()),
+    path('products/<slug:category_slug>/',views.CategoryDetail.as_view()),
+
 ]
